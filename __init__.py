a551de21e179ab8d4134917623614158a358b1b3
Raymond Nijssen <r.nijssen@terglobo.nl>
2018-09-18 22:18:20 +0200

    update for qgis3

--- __init__.py
+++ __init__.py
@@ -3,15 +3,14 @@
 # Copyright (C) 2015 Martin Dobias
 #-----------------------------------------------------------
 # Licensed under the terms of GNU GPL 2
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #---------------------------------------------------------------------
 
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
+from PyQt5.QtWidgets import QAction, QMessageBox
 
 def classFactory(iface):
     return MinimalPlugin(iface)
