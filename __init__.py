34610e1a4be0df6d022be188c962dfd2aef1bcb1
Étienne Trimaille <gustrimaille@yahoo.fr>
2022-04-19 11:56:43 +0200

    Add self.action in the init

--- __init__.py
+++ __init__.py
@@ -18,6 +18,7 @@ def classFactory(iface):
 class MinimalPlugin:
     def __init__(self, iface):
         self.iface = iface
+        self.action = None
 
     def initGui(self):
         self.action = QAction('Go!', self.iface.mainWindow())
